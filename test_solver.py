858a80e9475d116d732a234f0ec30366c4c71068
Jay <jaycode03@gmail.com>
2025-02-06 20:26:11 +0800

    delete pio connector files, and start to try out ML learning on xml hand history

--- test_solver.py
+++
@@ -1,8 +0,0 @@
-from pyosolver import PYOSolver
-
-
-solver = PYOSolver("J:\\PioSOLVER", "PioSOLVER2-edge")
-solver.load_tree("J:\\PioSOLVER\\saves\\hu_preflop.cfr")
-print(solver.show_node("r:0"))
-print(solver.show_children_actions("r:0"))
-#print(solver.show_range("IP","r:0"))
